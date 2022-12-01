92498c3e56a379271ba7dc7262ac35793f9e16bd
Stephan Pulver <stephan.pulver@gmail.com>
2022-12-01 11:17:36 +0100

    using example code

--- src/ca/lvin/books/jenkins/RandomWord.groovy
+++ src/ca/lvin/books/jenkins/RandomWord.groovy
@@ -1,25 +1,25 @@
 package ca.lvin.books.jenkins
 
 class RandomWord {
-  private def pipelineSteps
-  private List<String> words
-  private Random random
+    private def pipelineSteps
+    private List<String> words
+    private Random random
 
-  public RandomWord(pipelineSteps) {
-    this.pipelineSteps = pipelineSteps
-    this.words = this.readResourcesAsList("words.txt")
+    public RandomWord(pipelineSteps) {
+        this.pipelineSteps = pipelineSteps
+        this.words = this.readResourcesAsList("words.txt")
+        this.random = new Random()
+    }
 
-  }
+    public String get() {
+        int wordIndex = this.random.nextInt(this.words.size())
+        String capitalized = org.apache.commons.lang.WordUtils.capitalizeFully(this.words[wordIndex])
+        return capitalized
+    }
 
-  public String get() {
-    int wordIndex = this.random.nextInt(this.words.size())
-    String capitalized = org.apache.commons.lang.WordUtils.capitalizeFully(this.words[wordIndex])
-    return capitalized
-  }
-
-  @NonCPS
-  private List readResourcesAsList(String name) {
-    String resource = this.pipelineSteps.libraryResource(name)
-    return resource.split()
-  }
+    @NonCPS
+    private List readResourcesAsList(String name) {
+        String resource = this.pipelineSteps.libraryResource(name)
+        return resource.split()
+    }
 }
