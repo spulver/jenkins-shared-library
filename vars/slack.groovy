7f0696cc2ccb4efb119115c1989d71f962e9a75b
Stephan Pulver <stephan.pulver@gmail.com>
2022-12-02 09:17:32 +0100

    overload send

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,5 +1,9 @@
+void send(String channel, String message) {
+  send(channel: channel, message: message)
+}
+
 void send(Map params=[:]) {
-  
+
   Map defaultParams = [
     baseUrl: "https://tomboworkspacegroup.slack.com/services/hooks/jenkins-ci/",
     tokenCredentialId: "slack"
